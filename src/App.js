3325a28852ce82d71a5f8362ca34e0899ace89bd
zacharychoi-ce <zach7choi@gmail.com>
2021-10-18 00:31:34 +1300

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Title (change)</h1>
+    <div className='container'>
+      <Header />
     </div>
-  );
+  )
 }
 
 export default App;
