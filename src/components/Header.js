cc9508ef2ee8e179d3e9076e57da8bdf498ef15e
zacharychoi-ce <zach7choi@gmail.com>
2021-10-18 18:24:00 +1300

    Header component useLocation insert for hiding Add button in /about route

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
+import { useLocation } from 'react-router-dom' // for look at route we're currently on, and for Add button to not show on /about
 
 const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+
     // const onClick = () => {
     //     console.log('Clicked')
     // }
@@ -9,7 +12,8 @@ const Header = ({ title, onAdd, showAdd }) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+            {location.pathname === '/' && 
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />}
         </header>
     )
 }
